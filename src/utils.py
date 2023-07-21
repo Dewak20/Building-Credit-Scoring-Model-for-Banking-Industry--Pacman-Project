422a1718b086c4652be4b2f60b5b87a3b449674b
Dewak20 <dewakrishnadana90@gmail.com>
2023-07-21 21:48:43 +0800

    third commit

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import joblib
+import yaml
 
 
 def pickle_load(file_path):
@@ -8,3 +9,11 @@ def pickle_load(file_path):
 def pickle_dump(data, file_path):
     #Function to dump data into pivkle
     joblib.dump(data,file_path)
+
+
+def config_load():
+    #Function to load config files
+    
+    try:
+        with open(CONFIG DIR, 'r') as file:
+        
