93b541e340b3ed568e180d3f3088b7f6ad8dfa99
Dewak20 <dewakrishnadana90@gmail.com>
2023-07-24 13:13:20 +0800

    Fifth commit

--- src/utils.py
+++ src/utils.py
@@ -2,18 +2,26 @@ import joblib
 import yaml
 
 
+
+CONFIG_DIR = 'E:\PACMAN\Credit Score\config\config.yaml'
+
+
 def pickle_load(file_path):
     #Function to load pickle file
     return joblib.load(file_path)
 
 def pickle_dump(data, file_path):
-    #Function to dump data into pivkle
+    #Function to dump data into pivkle 
     joblib.dump(data,file_path)
 
 
 def config_load():
-    #Function to load config files
-    
+    """Function to load config files"""
     try:
-        with open(CONFIG DIR, 'r') as file:
-        
+        with open(CONFIG_DIR, 'r') as file:
+            config = yaml.safe_load(file)
+    except FileNotFoundError as error:
+        raise RuntimeError('Parameters file not found in path.')
+    
+    return config
+
